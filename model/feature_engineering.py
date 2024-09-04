3521a6ecd4bd9aa5d7dddcb0a226b29693a4f7be
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-09-05 00:34:27 +0600

    Model trained and saved

--- model/feature_engineering.py
+++ model/feature_engineering.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 default_directory = r"F:\Celebrity Face Recognition\model"
 os.chdir(default_directory)
 
+import warnings
+warnings.filterwarnings('ignore')
+
 # thanks to StackOverflow
 def w2d(img, mode='haar', level=1):
     imArray = img
@@ -65,3 +68,83 @@ for celebrity_name, training_files in celebrity_file_names_dict.items():
 X = np.array(X).reshape(len(X),4096).astype(float)
 print(X.shape)
 
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC(kernel = 'rbf', C = 10))])
+pipe.fit(X_train, y_train)
+print(pipe.score(X_test, y_test))
+print(classification_report(y_test, pipe.predict(X_test)))
+
+model_params = {
+    'svm': {
+        'model': svm.SVC(gamma='auto',probability=True),
+        'params' : {
+            'svc__C': [1,10,100,1000],
+            'svc__kernel': ['rbf','linear']
+        }  
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params' : {
+            'randomforestclassifier__n_estimators': [1,5,10]
+        }
+    },
+    'logistic_regression' : {
+        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
+        'params': {
+            'logisticregression__C': [1,5,10]
+        }
+    }
+}
+
+scores = []
+best_estimators = {}
+import pandas as pd
+for algo, mp in model_params.items():
+    pipe = make_pipeline(StandardScaler(), mp['model'])
+    clf =  GridSearchCV(pipe, mp['params'], cv=5, return_train_score=False)
+    clf.fit(X_train, y_train)
+    scores.append({
+        'model': algo,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+    best_estimators[algo] = clf.best_estimator_
+    
+df = pd.DataFrame(scores,columns=['model','best_score','best_params'])
+print(df)
+
+print(best_estimators['svm'].score(X_test,y_test))
+print(best_estimators['random_forest'].score(X_test,y_test))
+print(best_estimators['logistic_regression'].score(X_test,y_test))
+
+best_clf = best_estimators['logistic_regression']
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, best_clf.predict(X_test))
+print(cm)
+
+import seaborn as sn
+plt.figure(figsize = (10,7))
+sn.heatmap(cm, annot=True)
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+# plt.show()
+
+import joblib 
+joblib.dump(best_clf, 'saved_model.pkl') 
+
+import json
+with open("class_dictionary.json","w") as f:
+    f.write(json.dumps(class_dict))
